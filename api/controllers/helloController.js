a54a3007ff80d2df0f798cc12c5525ca9809b602
henry.mhene <henry.mhene@oisoftware.com.au>
2018-08-22 10:19:33 +1000

    dynamic commit hash added

--- api/controllers/helloController.js
+++ api/controllers/helloController.js
@@ -1,5 +1,11 @@
 'use strict';
 
+let commithash = 'abc57858585';
+
+require('child_process').exec('git rev-parse HEAD', function(err, res) {
+    commithash = res;
+});
+
 exports.hello_world = function (req, res) {
     res.json('hello world');
 };
@@ -15,9 +21,9 @@ exports.metadata = function (req, res) {
                 "version": "1.0"
             },
             {
-                "description": "pre-interview technical test",
+                "description": "henry mhene`s technical test",
             }, {
-                "lastcommitsha": "abc57858585"
+                "lastcommitsha": commithash
             }
         ],
     };
